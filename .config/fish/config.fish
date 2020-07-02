e7df90a37ef22e990aabf8b3455bfbbb98b302d9
Alexey Boklin <alex.boklin@gmail.com>
2020-07-02 23:49:00 +0200

    Set TODOs for fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,16 @@
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
 
-alias clean_branches='git branch | grep -v "master" | xargs git branch -D'
+# TODO: change the alias.
+alias delete_non-master_branches='git checkout master && git branch | grep -v "master" | xargs git branch -D'
 
 fish_vi_key_bindings
 
 starship init fish | source
+
+# TODO: find a way to use it inside .vimrc.
+export VIM_SESSION_DIR=~/.config/nvim/session/.vim
+
+# TODO: alias for cleaning up the sessions. 
+
+# TODO: aliases for config commands.
