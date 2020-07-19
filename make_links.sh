eb83cbba9562bc6e51a7214db92540a780581f5d
Alexey Boklin <alex.boklin@gmail.com>
2020-07-19 13:18:43 +0200

    Use variables for symbolic link

--- make_links.sh
+++ make_links.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-ln -s ~/Git/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+#!/bin/bash
+GIT_FOLDER="~/Git/dotfiles/.config"
+ORIGINAL_FOLDER="~/.config"
+
+#-ln -s ~/Git/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+bash -c "ln -sf $GIT_FOLDER/nvim/init.vim $ORIGINAL_FOLDER/nvim/init.vim"
