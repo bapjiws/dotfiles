04b5d85171fc6649dff8760fdda923e0cef26cf9
Alexey Boklin <alex.boklin@gmail.com>
2020-07-20 11:23:40 +0200

    Set up links for plugin configs

--- make_links.sh
+++ make_links.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-GIT_FOLDER="~/Git/dotfiles/.config"
-ORIGINAL_FOLDER="~/.config"
+GIT_FOLDER=~/Git/dotfiles/.config
+ORIGINAL_FOLDER=~/.config
 
-#-ln -s ~/Git/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-bash -c "ln -sf $GIT_FOLDER/nvim/init.vim $ORIGINAL_FOLDER/nvim/init.vim"
+PLUGIN_CONFIG_FOLDER=nvim/plugin-config
+
+ln -sf $GIT_FOLDER/nvim/init.vim $ORIGINAL_FOLDER/nvim/init.vim
+
+for file in $(ls $GIT_FOLDER/nvim/plugin-config)
+do
+  ln -sf $GIT_FOLDER/$PLUGIN_CONFIG_FOLDER/$file $ORIGINAL_FOLDER/$PLUGIN_CONFIG_FOLDER/$file
+done
