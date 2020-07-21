0a046c9f360db3c0632b0ecf2ea0074971b95a7e
Alexey Boklin <alex.boklin@gmail.com>
2020-07-21 10:05:00 +0200

    Use folder variable in loop

--- make_links.sh
+++ make_links.sh
@@ -6,7 +6,7 @@ PLUGIN_CONFIG_FOLDER=nvim/plugin-config
 
 ln -sf $GIT_FOLDER/nvim/init.vim $ORIGINAL_FOLDER/nvim/init.vim
 
-for file in $(ls $GIT_FOLDER/nvim/plugin-config)
+for file in $(ls $GIT_FOLDER/$PLUGIN_CONFIG_FOLDER)
 do
   ln -sf $GIT_FOLDER/$PLUGIN_CONFIG_FOLDER/$file $ORIGINAL_FOLDER/$PLUGIN_CONFIG_FOLDER/$file
 done
