df7eba4ce46cb1730f8433bcc474d23cf999b059
Alexey Boklin <alex.boklin@gmail.com>
2020-08-08 23:03:30 +0200

    Make symbolic links for themes as well

--- make_links.sh
+++ make_links.sh
@@ -3,10 +3,16 @@ GIT_FOLDER=~/Git/dotfiles/.config
 ORIGINAL_FOLDER=~/.config
 
 PLUGIN_CONFIG_FOLDER=nvim/plugin-config
+THEME_CONFIG_FOLDER=nvim/themes
 
 ln -sf $GIT_FOLDER/nvim/init.vim $ORIGINAL_FOLDER/nvim/init.vim
 
 for file in $(ls $GIT_FOLDER/$PLUGIN_CONFIG_FOLDER)
 do
   ln -sf $GIT_FOLDER/$PLUGIN_CONFIG_FOLDER/$file $ORIGINAL_FOLDER/$PLUGIN_CONFIG_FOLDER/$file
+done
+
+for file in $(ls $GIT_FOLDER/$THEME_CONFIG_FOLDER)
+do
+  ln -sf $GIT_FOLDER/$THEME_CONFIG_FOLDER/$file $ORIGINAL_FOLDER/$THEME_CONFIG_FOLDER/$file
 done
