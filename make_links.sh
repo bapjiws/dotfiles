59ec3fb89b604498865cfec4dd5ffc738ca8402e
Alexey Boklin <alex.boklin@gmail.com>
2021-01-14 02:25:17 +0100

    Add and link Ranger config

--- make_links.sh
+++ make_links.sh
@@ -7,6 +7,7 @@ THEME_CONFIG_FOLDER=nvim/themes
 
 ln -sf $DOTFILES_FOLDER/nvim/init.vim $ORIGINAL_FOLDER/nvim/init.vim
 ln -sf $DOTFILES_FOLDER/.zshrc ~/.zshrc
+ln -sf $DOTFILES_FOLDER/ranger/rc.conf $ORIGINAL_FOLDER/ranger/rc.conf
 ln -sf $DOTFILES_FOLDER/UltiSnips/javascript.snippets $ORIGINAL_FOLDER/nvim/UltiSnips/javascript.snippets
 
 for file in $(ls $DOTFILES_FOLDER/$PLUGIN_CONFIG_FOLDER)
