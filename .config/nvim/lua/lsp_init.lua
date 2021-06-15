21ee13a2e65c4d4e7e74e0a38e033856e7ffc84d
Alexey Boklin <alex.boklin@gmail.com>
2021-06-15 20:05:19 +0200

    Remove comment

--- .config/nvim/lua/lsp_init.lua
+++ .config/nvim/lua/lsp_init.lua
@@ -35,7 +35,7 @@ local function on_attach(client)
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
     buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-    -- Mappings.
+
     local opts = {noremap = true, silent = true}
 
     buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
