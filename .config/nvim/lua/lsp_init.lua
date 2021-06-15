27df199cdc8def2b34fea3ebe6c823785ca704ce
Alexey Boklin <alex.boklin@gmail.com>
2021-06-15 20:05:58 +0200

    Remove setting omnifunc

--- .config/nvim/lua/lsp_init.lua
+++ .config/nvim/lua/lsp_init.lua
@@ -32,9 +32,6 @@ vim.fn.sign_define("LspDiagnosticsSignHint", { text = "", numhl = "LspDiagnos
 
 local function on_attach(client)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
     local opts = {noremap = true, silent = true}
 
