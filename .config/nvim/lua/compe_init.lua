901632507b069656eeea916f8e48e129801cbf2b
Alexey Boklin <alex.boklin@gmail.com>
2021-06-15 20:09:06 +0200

    Clean compe sources

--- .config/nvim/lua/compe_init.lua
+++ .config/nvim/lua/compe_init.lua
@@ -15,10 +15,8 @@ require'compe'.setup {
   source = {
     path = true;
     buffer = true;
-    calc = true;
     nvim_lsp = true;
     nvim_lua = true;
-    vsnip = false;
     ultisnips = true;
   };
 }
