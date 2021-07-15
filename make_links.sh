dc7e5c13f1c4504b33058ce881ed31c4688f6a5c
Alexey Boklin <alex.boklin@gmail.com>
2021-07-15 23:25:53 +0200

    Symlink to new starship config location

--- make_links.sh
+++ make_links.sh
@@ -6,5 +6,6 @@ ln -sf $DOTFILES_FOLDER/nvim $ORIGINAL_FOLDER/nvim
 ln -sf $DOTFILES_FOLDER/UltiSnips/javascript.snippets $ORIGINAL_FOLDER/nvim/UltiSnips/javascript.snippets
 
 ln -sf $DOTFILES_FOLDER/ranger/rc.conf $ORIGINAL_FOLDER/ranger/rc.conf
+
 ln -sf $DOTFILES_FOLDER/fish/config.fish $ORIGINAL_FOLDER/fish/config.fish
-ln -sf $DOTFILES_FOLDER/starship.toml $ORIGINAL_FOLDER/starship.toml
+ln -sf $DOTFILES_FOLDER/fish/starship.toml $ORIGINAL_FOLDER/starship.toml
