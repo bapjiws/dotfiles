7848b4644eac4af76c8810bc0eb74d0bf7119d66
Alexey Boklin <alex.boklin@gmail.com>
2021-09-15 16:09:51 +0200

    Rework gitsigns config

--- .config/nvim/lua/gitsigns_init.lua
+++ .config/nvim/lua/gitsigns_init.lua
@@ -1,18 +1,5 @@
 require('gitsigns').setup {
-  signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-  },
-  numhl = false,
-  linehl = false,
   keymaps = {
-    -- Default keymap options
-    noremap = true,
-    buffer = true,
-
     ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
     ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
 
@@ -20,20 +7,11 @@ require('gitsigns').setup {
     ['n <leader>rst'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
     ['n <leader>prw'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
     ['n <leader>bll'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+    ['n <leader>shk'] = '<cmd>lua require"gitsigns".select_hunk()<CR>',
+
     -- TODO: try
     --['n <leader>bll'] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
-
-    -- Text objects
-    ['o ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
-    ['x ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>'
   },
-  watch_index = {
-    interval = 1000
-  },
-  current_line_blame = false,
-  sign_priority = 6,
-  update_debounce = 100,
-  status_formatter = nil, -- Use default
-  use_decoration_api = true,
-  use_internal_diff = true,  -- If luajit is present
+  -- TODO: test later, doesn't seem to work right now.
+  current_line_blame = true,
 }
