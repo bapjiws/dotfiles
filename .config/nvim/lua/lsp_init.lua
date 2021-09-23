22e57e6676056502f218cc03ff3867e11db80260
Alexey Boklin <alex.boklin@gmail.com>
2021-09-23 14:02:57 +0200

    Remove autocomplete symbols from lsp init

--- .config/nvim/lua/lsp_init.lua
+++ .config/nvim/lua/lsp_init.lua
@@ -140,32 +140,3 @@ lsp_config.efm.setup({
     languages = efm_languages
   }
 })
-
--- symbols for autocomplete
-vim.lsp.protocol.CompletionItemKind = {
-    "   (Text) ",
-    "   (Method)",
-    "   (Function)",
-    "   (Constructor)",
-    " ﴲ  (Field)",
-    "[] (Variable)",
-    "   (Class)",
-    " ﰮ  (Interface)",
-    "   (Module)",
-    " 襁 (Property)",
-    "   (Unit)",
-    "   (Value)",
-    " 練 (Enum)",
-    "   (Keyword)",
-    "   (Snippet)",
-    "   (Color)",
-    "   (File)",
-    "   (Reference)",
-    "   (Folder)",
-    "   (EnumMember)",
-    " ﲀ  (Constant)",
-    " ﳤ  (Struct)",
-    "   (Event)",
-    "   (Operator)",
-    "   (TypeParameter)"
-}
