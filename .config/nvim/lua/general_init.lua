ea3e34ddf9f653123461025ec7985de3f8ff00bb
Alexey Boklin <alex.boklin@gmail.com>
2022-02-03 13:19:06 +0100

    Delete existing files

--- .config/nvim/lua/general_init.lua
+++
@@ -1,7 +0,0 @@
-local map = vim.api.nvim_set_keymap
-
--- map the leader key
-map('n', '<Space>', '', {})
-vim.g.mapleader = ' '  -- 'vim.g' sets global variables
-
-vim.cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'  -- disabled in visual mode
--- .config/nvim/lua/sonokai_init.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd([[
-if has('termguicolors')
-  set termguicolors
-endif
-]])
-
-vim.g.sonokai_style = 'andromeda'
-
-vim.cmd("colorscheme sonokai")
