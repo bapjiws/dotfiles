4365313cdb82e6b05674d20f6dc68a8b8802b834
Alexey Boklin <alex.boklin@gmail.com>
2022-02-10 11:53:02 +0100

    Fix TODO message in options

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -40,7 +40,7 @@ vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
 
--- TODO: My mappings
+-- TODO: My options
 
 -- set termguicolors
 -- set list lcs=tab:\ \ 
