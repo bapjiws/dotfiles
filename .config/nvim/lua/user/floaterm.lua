a77090d617e1fce27c92025799f7ac5cb4cb9f15
Alexey Boklin <alex.boklin@gmail.com>
2022-08-22 14:39:35 +0200

    Update floaterm

--- .config/nvim/lua/user/floaterm.lua
+++ .config/nvim/lua/user/floaterm.lua
@@ -2,4 +2,4 @@ vim.cmd("let g:floaterm_opener='vsplit'")
 vim.cmd("let g:floaterm_width=0.8")
 vim.cmd("let g:floaterm_height=0.8")
 
-vim.api.nvim_set_keymap('n', '<leader>vsc', ':FloatermNew lazygit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>vct', ':FloatermNew lazygit<CR>', { noremap = true, silent = true })
