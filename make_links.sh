6c794ee4ae7096270f76744f4e4121b47f0b120f
Alexey Boklin <alex.boklin@gmail.com>
2023-03-19 00:37:18 +0100

    Set up fnm auto switch

--- make_links.sh
+++ make_links.sh
@@ -6,6 +6,7 @@ ln -sf $DOTFILES_FOLDER/nvim $ORIGINAL_FOLDER/nvim
 ln -sf $DOTFILES_FOLDER/UltiSnips $ORIGINAL_FOLDER/nvim/UltiSnips
 
 ln -sf $DOTFILES_FOLDER/ranger/rc.conf $ORIGINAL_FOLDER/ranger/rc.conf
+ln -sf $DOTFILES_FOLDER/fish/conf.d/fnm.fish $ORIGINAL_FOLDER/fish/conf.d/fnm.fish
 
 ln -sf $DOTFILES_FOLDER/fish/config.fish $ORIGINAL_FOLDER/fish/config.fish
 ln -sf $DOTFILES_FOLDER/fish/functions/nvm.fish $ORIGINAL_FOLDER/fish/functions/nvm.fish
