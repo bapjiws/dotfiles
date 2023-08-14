f45b3b5f156abfd6572d61c1b806fb3c93e44820
Alexey Boklin <alex.boklin@gmail.com>
2023-08-14 12:44:13 +0200

    Update nvim-tree setup

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -1,19 +1,27 @@
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
-  return
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-nvim_tree.setup {
+require("nvim-tree").setup({
+  on_attach = my_on_attach,
   update_focused_file = {
     enable = true,
     update_cwd = true,
+    update_root = true,
   },
   renderer = {
     -- root_folder_modifier = ":t",
@@ -56,16 +64,7 @@ nvim_tree.setup {
   view = {
     width = 30,
     side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
   },
-}
+})
 
-local opts = {noremap = true, silent = true}
-
-vim.api.nvim_set_keymap('n', '<leader>fex', ':NvimTreeFindFile<CR>', opts)
+vim.keymap.set('n', '<leader>fex', ':NvimTreeFindFile<CR>', {noremap = true, silent = true})
