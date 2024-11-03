898f17d7769258f2317dcb7ca1cd35f42bf3e758
Alexey Boklin <alex.boklin@gmail.com>
2024-11-03 13:10:23 +0100

    Add trm binding

--- .config/nvim/lua/user/floaterm.lua
+++ .config/nvim/lua/user/floaterm.lua
@@ -4,3 +4,4 @@ vim.cmd("let g:floaterm_height=0.8")
 
 vim.api.nvim_set_keymap('n', '<leader>vct', ':FloatermNew lazygit<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fex', ':FloatermNew ranger<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>trm', ':FloatermNew fish<CR>', { noremap = true, silent = true })
