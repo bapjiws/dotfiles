a4a309340a4b1dfb98e4d493906fd53d61a12774
Alexey Boklin <alex.boklin@gmail.com>
2025-08-31 13:43:11 +0200

    Fix bug in fqp

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -29,7 +29,7 @@ vim.api.nvim_set_keymap('n', '<leader>fwf', [[<cmd>lua require('telescope.builti
 
 vim.api.nvim_set_keymap('n', '<leader>sip', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], opts)
 vim.api.nvim_set_keymap('n', '<leader>fwp', [[<cmd>lua require('telescope.builtin').live_grep({ default_text = vim.fn.expand("<cword>") })<CR>]], opts)
-vim.api.nvim_set_keymap('n', '<leader>fqp', [[<cmd>lua require('telescope.builtin').live_grep({ default_text = vim.fn.getreg('"') })<CR>]], opts)
+vim.api.nvim_set_keymap('n', '<leader>fqp', [[<cmd>lua local line = vim.fn.getline(".") local start = string.find(line, '"[^"]*"') if start then local end_pos = string.find(line, '"', start + 1) if end_pos then local text = string.sub(line, start + 1, end_pos - 1) require('telescope.builtin').live_grep({ default_text = text }) else require('telescope.builtin').live_grep() end else require('telescope.builtin').live_grep() end<CR>]], opts)
 
 vim.api.nvim_set_keymap('n', '<Leader>sib', [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], opts)
 
