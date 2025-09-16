41c22914c802852454f53d9d48477feb5cb372a1
Alexey Boklin <alex.boklin@gmail.com>
2025-09-16 13:53:09 +0200

    Map S-Tab for Copilot completion

--- .config/nvim/lua/user/copilot.lua
+++ .config/nvim/lua/user/copilot.lua
@@ -14,3 +14,6 @@ vim.keymap.set('n', '<leader>cbc', function()
     require("CopilotChat").ask("#buffer " .. input)
   end
 end, { desc = "CopilotChat - Quick chat" })
+
+vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
