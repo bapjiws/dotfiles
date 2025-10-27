e8f0133062b9c7de49cf3937785ac430a76e1bba
Alexey Boklin <alex.boklin@gmail.com>
2025-10-27 21:36:17 +0100

    Mappings for chat and chat new

--- .config/nvim/lua/user/avante.lua
+++ .config/nvim/lua/user/avante.lua
@@ -2,4 +2,5 @@ require('avante').setup({
   provider = "copilot"
 })
 
-vim.api.nvim_set_keymap('n', '<leader>cc', ":AvanteChat<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ccc', ":AvanteChat<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ccn', ":AvanteChatNew<CR>", { noremap = true, silent = true })
