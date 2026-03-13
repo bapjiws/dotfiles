126d2495ff2616a13c9469f925a2f16fd17fffe1
bapjiws <alexey.boklin@protonmail.com>
2026-03-13 14:41:45 +0100

    Use yabai in floating mode

--- .config/yabai/left50.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if yabai -m query --windows --window | jq -e '.["is-floating"]' | grep -q true; then
-    yabai -m window --grid 1:2:0:0:1:1
-else
-    yabai -m window --toggle float && yabai -m window --grid 1:2:0:0:1:1
-fi
