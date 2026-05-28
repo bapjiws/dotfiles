40605b132228fa7e0cd84305dc6e5a7c7f86da5a
bapjiws <alexey.boklin@protonmail.com>
2026-05-28 12:32:52 +0200

    Fix depreacted sign_define

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -10,20 +10,16 @@ M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
 M.setup = function()
-  local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
-  }
-
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-  end
-
   vim.diagnostic.config({
     virtual_text = true,
-    signs = true,
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = "",
+        [vim.diagnostic.severity.WARN]  = "",
+        [vim.diagnostic.severity.HINT]  = "",
+        [vim.diagnostic.severity.INFO]  = "",
+      },
+    },
     update_in_insert = false,
     underline = true,
     severity_sort = true,
