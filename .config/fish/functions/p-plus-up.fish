77b8c33caf4c6ccd8d037fb8421bc894368c27da
Alexey <alexey.boklin@protonmail.com>
2026-08-13 14:55:05 +0200

    Open adjacent tabs

--- .config/fish/functions/p-plus-up.fish
+++ .config/fish/functions/p-plus-up.fish
@@ -1,15 +1,16 @@
-function p-plus-up --description "Open a Ghostty window with the P+ portal + service dev stack running in separate tabs"
+function p-plus-up --description "Open 3 new tabs in the current Ghostty window running the P+ portal + service dev stack"
     set -l portal_dir ~/git/p-plus/p-plus-portal
     set -l service_dir ~/git/p-plus/p-plus-service
 
     osascript \
         -e 'tell application "Ghostty"' \
-        -e 'activate' \
+        -e 'set win to front window' \
+        -e 'set originalTab to selected tab of win' \
         -e 'set cfg1 to new surface configuration' \
         -e "set initial working directory of cfg1 to \"$portal_dir\"" \
         -e 'set command of cfg1 to "npm run dev"' \
         -e 'set wait after command of cfg1 to true' \
-        -e 'set win to new window with configuration cfg1' \
+        -e 'new tab in win with configuration cfg1' \
         -e 'set cfg2 to new surface configuration' \
         -e "set initial working directory of cfg2 to \"$portal_dir\"" \
         -e 'set command of cfg2 to "npx overmind-devtools@latest"' \
@@ -20,5 +21,6 @@ function p-plus-up --description "Open a Ghostty window with the P+ portal + ser
         -e 'set command of cfg3 to "npm run dev"' \
         -e 'set wait after command of cfg3 to true' \
         -e 'new tab in win with configuration cfg3' \
+        -e 'select tab originalTab' \
         -e 'end tell'
 end
